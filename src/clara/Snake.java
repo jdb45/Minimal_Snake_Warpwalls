7f4b2b80bd0f77f4fd7106d4d7bc04516fe4a63c
Jeremy <acnajcadcac.yahoo.com>
2016-10-30 14:30:28 -0500

    Warpwalls working Changed snake, kibble, and game messages to different colors. Changed the screen size.

--- src/clara/Snake.java
+++ src/clara/Snake.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class Snake extends TimerTask implements KeyListener {
 
-    int height = 300; int width = 400;   //pixels
+    int height = 500; int width = 600;   //pixels
     int squareSize = 50;
 
     int xSquares = width/squareSize;
@@ -71,13 +71,13 @@ public class Snake extends TimerTask implements KeyListener {
             g.fillRect(0, 0, width, height);
 
             if (gameOver > 6) {                 // If gameOver indicates game is won, display message
-                g.setColor(Color.GREEN);
+                g.setColor(Color.PINK);
                 g.drawString(">-o~~~~~~~~~~~~~  SNAKE  ~~~~~~~~~~~~~o-<", 50, 50);    //  "art"
                 g.drawString("!!!! YOU WON !!! score: " + score, 100, 100);
             }
 
             else if (gameOver > 0 ) {          // If gameOver indicates game is over (won, lost, whatever) display score and countdown to next game
-                g.setColor(Color.GREEN);
+                g.setColor(Color.PINK);
                 g.drawString(">-o~~~~~~~~~~~~~  SNAKE  ~~~~~~~~~~~~~o-<", 50, 50);
 
                 g.drawString("GAME OVER score: " + score, 120, 100);
@@ -86,10 +86,10 @@ public class Snake extends TimerTask implements KeyListener {
             }
 
             else {                             // Game is not over. Draw snake and kibble, wherever they are.
-                g.setColor(Color.BLUE);
+                g.setColor(Color.CYAN);
                 g.fillRect(kibble[0] * squareSize, kibble[1] * squareSize, squareSize, squareSize);
 
-                g.setColor(Color.RED);
+                g.setColor(Color.MAGENTA);
                 for (int[] square : snake) {
                     g.fillRect(square[0] * squareSize, square[1] * squareSize, squareSize, squareSize);
                 }
@@ -135,10 +135,27 @@ public class Snake extends TimerTask implements KeyListener {
             headX = newHead[0];    //Convenience variables for new head x and y
             headY = newHead[1];
 
-            if ((headX < 0 || headX > xSquares) || (headY < 0 || headY > ySquares)) {   //Head outside board? Snake hit wall, game over
+           /* if ((headX < 0 || headX > xSquares) || (headY < 0 || headY > ySquares)) {   //Head outside board? Snake hit wall, game over
                 gameOver = clockTicksToRestart;
                 return;
-            }
+            }*/
+                //added warp walls  by resetting the variables of snake and head to 0
+                if (headX > xSquares) {
+                    headX = 0;
+                    snake.get(0)[0] = 0;
+
+                } else if (headX < 0) {
+                    headX = xSquares - 1;
+                    snake.get(0)[0] = xSquares - 1;
+
+                } else if (headY > ySquares) {
+                    headY = 0;
+                    snake.get(0)[1] = 0;
+
+                } else if (headY < 0) {
+                    headY = ySquares - 1;
+                    snake.get(0)[1] = ySquares - 1;
+                }
 
             if (headX == kibble[0] && headY == kibble[1]) {      //Is kibble in same square as snake head? Snake ate kibble.
                 score++;                              // increase score
